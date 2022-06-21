2d172720cb16d86b612e716fc564a226815e7011
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2022-06-21 18:56:19 +0300

    0.0.2

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pyttsx3
+import fitz
 
 s = pyttsx3.init()
-data = "Это обычный текст"
-s.say(data)
+
+with fitz.open("Programming task.pdf") as doc:
+    text = ''
+    for page in doc.pages(3, 219):
+        text += page.get_text()
+
+s.say(text)
 s.runAndWait()
